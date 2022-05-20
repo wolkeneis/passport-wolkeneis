7d7bd515d052620893c5dd302307a4fc7c282967
Simon <simon@benezan.de>
2022-05-20 17:49:57 +0200

    fix linting and use moos-api types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Request } from "express-serve-static-core";
 import OAuth2Strategy, { InternalOAuthError, StrategyOptionsWithRequest, VerifyFunctionWithRequest } from "passport-oauth2";
+import { v1 } from "moos-api";
 
 export interface WolkeneisStrategyOptionsWithRequest extends StrategyOptionsWithRequest {
   userProfileUrl: string;
@@ -19,30 +20,27 @@ export default class WolkeneisStrategy extends OAuth2Strategy {
     this._userProfileUrl = options.userProfileUrl || "https://moos.wolkeneis.dev/api/v1/application/profile";
   }
 
-  authenticate(request: Request, options?: any) {
+  authenticate(request: Request, options?: unknown) {
     if (request.query && request.query.denied) {
       return this.fail();
     }
     super.authenticate(request, options);
   }
 
-  userProfile(accessToken: string, done: (error?: Error | null, profile?: any) => void): void {
+  userProfile(accessToken: string, done: (error?: Error | null, profile?: v1.UserProfile) => void): void {
     this._oauth2.get(this._userProfileUrl, accessToken, (error, body) => {
       if (error) {
         return done(new InternalOAuthError("Failed to fetch the user profile.", error));
       }
 
-      let data: any;
+      let data: unknown;
       try {
         data = JSON.parse(body as string);
       } catch (error) {
         return done(new Error("Failed to parse the user profile."));
       }
 
-      const profile = data;
-      profile.provider = "wolkeneis";
-      profile.fetchedAt = Date.now();
-      return done(null, profile);
+      return done(null, data as v1.UserProfile);
     });
   }
 }
